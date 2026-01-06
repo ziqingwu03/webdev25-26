60f07dc26c1496940c956aadecc567e2bf6876f8
ziqingwu03 <Ziqingw3@nycstudents.net>
2026-01-06 19:08:32 +0000

    ww

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,3 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function img(corgi) {
+    document.getElementById('highlight').innerHTML= corgi 

78b3b4496ed1b289e7ed8a824c7101ffd17f8ca0
ziqingwu03 <Ziqingw3@nycstudents.net>
2026-02-06 19:06:55 +0000

    www

--- WDLesson4_2Practical/script.js
+++ WDLesson4_2Practical/script.js
@@ -13,13 +13,25 @@ function name(){
 */
 
 /* Challenge 2:  Create the function for Challenge 1 scenario */
+function area() {
+  let w = 15;
+  let h = 10;
+  let n1= parseFloat(document.getelementsById("num1"))
+  let n2= parseFloat(document.getelementsById("num1"))
+  let n3= parseFloat(document.getelementsById("a"))
+  let n3 = calculatearea(w*h);
+
+}
 
 
 
-/* Challenge 3:  Complete the function for Pets Challenge on pets.html.  
+/* Challenge 3:  Complete the function for Pets Challenge on pets.html.
+
 1) Review the HTML for the UI.  Remember dropdown boxes are just input.
 2) You will need to write HTML as part of the output.  
    For example, output.innerHTML = `<input src="pic.png">` would place the image in the <div>*/
 function pet(){
+
+
 
 }

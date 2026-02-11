fcd6204329ac8fb3e021daff0c4842543a410745
ziqingwu03 <Ziqingw3@nycstudents.net>
2026-02-11 19:05:52 +0000

    www

--- WDLesson4_3Practical/script.js
+++ WDLesson4_3Practical/script.js
@@ -3,6 +3,30 @@
        2) Display BMI status as per the chart in index.html
        3) Display the corresponding image based on the BMI status.
 */
+       function BMI() {
+          let wt =parseFloat (document.getElemetById("weight").value);
+          let ht =parseFloat (document.getElemetById("height").value);
+          let output =document.getElementById("output")
+          let status =" ";
+          let imagefilename=" ";
+          let BMI = 703 * wt/(ht)**2
+              if(BMI < 18.5){
+                     status ="udnerweight";
+                     filename="underweight.jpg";
+              } else if(BMI >= 18.5 && BMI <=24.9){
+                     status ="Healthy Weight";
+                     filename ="healthyweight.png";
+
+              } else if (BMI >= 25 && BMI <=29.9) {
+                     status= "overweight";
+                     filename= "overweight.png";
+              } else if (BMI >=30){
+                     status ="Obesity";
+                     filename="obeseweight.png";
+              }
+              
+              
+       }
 /* 
 Guideline:
 1) Create the function

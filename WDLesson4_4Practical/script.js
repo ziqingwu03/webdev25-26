26e5304d7b44af21ee298cd83814096f9f556a38
ziqingwu03 <Ziqingw3@nycstudents.net>
2026-02-26 18:57:11 +0000

    www

--- WDLesson4_4Practical/script.js
+++ WDLesson4_4Practical/script.js
@@ -4,12 +4,20 @@
        3) Store the correct answer in the variable correct.  This will be used in checkProblem( )
        4) Using string interpolation, display an addition problem like "2 + 2 =" except using the random numbers generated. 
 */
+
+
 let correct;
 function generateProblem(){
+let num1 = Math.floor(Math.random()*(20-0+1)+0);
+let num2 = Math.floor(Math.random()*(20-0+1)+0);
+let correct =num1 + num2; 
+
+
+
 
 }
 
-/* Challenge 3: Complete the function checkProblem by ,
+/* Challenge 3: Complete the function checkResponse  by ,
        1) Create a variable for the users response
        2) Create a variable for the <div> with id of output
        3) Create a variable msg for the output
@@ -18,8 +26,15 @@ function generateProblem(){
            b) otherwise set msg to "Incorrect. The answer is " followed by the correct answer.  Also include incorrect image
        4) Display msg in the output
 */
-function checkProblem(){
-
-}
-
+function checkResponse(){
+       userResponse = document.getElementById();
+       output = document.getElementById("output");
+       msg =" ";
+       correctAnswer = "13";
+if (userResponse && userResponse.value == correctAnswer) {
+        msg = "correct! 'src='correct.webp'>";
+    } else {
+        msg = "Incorrect. The answer is " + correctAnswer {
+              "<img src='incorrect.png'>";
+        }
 

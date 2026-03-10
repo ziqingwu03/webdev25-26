e6cd70ba638dad5a7990da570ccf568b63ee5bbe
ziqingwu03 <Ziqingw3@nycstudents.net>
2026-03-10 18:02:03 +0000

    www

--- WDLesson5_2Practical/script.js
+++ WDLesson5_2Practical/script.js
@@ -1,21 +1,33 @@
 /* Challenge 2: Complete the init() function so that you build HTML output as per the wireframe diagram image.
 Notes:
-1) Images
+1)Give each info card div a class of "card"
+2) Images
    a. Titles and Pizza are both images stored in the images folder.
    b. When creating the images give each a class of either "title" or "pizza" respectively
    c. Each image has been conveniently labeled with a number at the end in order to use a 
-      loop to retrieve the images.
-2) Create an appropriate random price to display for each pizza
-3) Enter quantity is a text input
-4) Buy is a button input
+               loop to retrieve the images.
+3) Create an appropriate random price to display for each pizza (say 15 to 25)No html elements needed 
+4) Enter quantity is a text input (no need to give it an id )
+5) Buy is a button input (no need to give it an event listener)
 
 Now that you have the template, use a for loop to produce all five pizza cards.
 */ 
 
-// Go to style.css for Challenge 3
+// Go to style.css for Challenge 3-6
 
 //init() function is called when the page loads in index.html
 function init(){
+   <div class="card">
+document.getElementById("myImage").src = "logo.png";
+   
+   
+   </div>
+
+let logo=document.getElementById("pizza1").src = "image/logo.png";
+let pizza1=document.getElementById("pizza1").src = "image/pizza1.jpg";
+let pizza2=document.getElementById("pizza2").src = "image/pizza2.jpg";
+
+
   
 
 }

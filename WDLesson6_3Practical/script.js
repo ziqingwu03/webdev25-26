0846bfdc73b1b8aadaa42f9a36639476e3006c20
ziqingwu03 <Ziqingw3@nycstudents.net>
2026-04-15 18:00:40 +0000

    www

--- WDLesson6_3Practical/script.js
+++ WDLesson6_3Practical/script.js
@@ -1,8 +1,7 @@
 /* Challenges 4,5, and 6 below task you to transform the following parallel arrays into arrays of JSON
 let meat_titles = ["Beef Tips","Brisket","Ribeye","Steak"]
 let meat_images = ["beeftips.jpg","brisket.jpg","ribeye.jpg","steak.jpg"];
-let meat_prices = ["18.99","27.99","21.99","15.99"];
-
+let meat_prices = [18.99,27.99,21.99,15.99];
 
 let seafood_titles = ["Clams","Crabs","Lobster","Scallop","Shrimp","Tuna"]
 let seafood_images = ["clams.jpg","crabs.jpg","lobster.png","scallops.jpg","shrimp.jpg","tuna.jpg"];
@@ -12,33 +11,30 @@ let dessert_titles = ["Flan","Passion Berry","Oreo Cup"]
 let dessert_images = ["flan.jpg","passionberry.jpeg","oreocup.jpg"];
 let dessert_prices = [4.15,3.99,2.99];
 */
-let meats = [
-            { title: "Beef Tips", image: "beeftips.jpg", price: 18.99 },
-            { title: "Brisket", image: "brisket.jpg", price: 27.99 },
-            { title: "Ribeye", image: "ribeye.jpg", price: 21.99 },
-            { title: "Steak", image: "steak.jpg", price: 15.99 }
-          ] ;
-          
-let seafood = [
-            {tittle: "Clams", "image": "clams.jpg", "price": 8.99},
-            {tittle: "Crabs", "image": "carbs.jpg", "price": 12.99},
-            {tittle: "Lobster", "image":"lobster.png", "price" :30.59},
-            {tittle: "Scallop", "image":"scallops.jpg", "price": 23.95},
-            {tittle: "Shrimp", "image":"shrimp.png", "price" :10.99},
-            {tittle: "Tuna", "image":"tuna.jpg", "price": 5.99}
-          ];
-
-let dessert = [
-            {tittle: "Flan", "image": "flan.jpg", "price": 4.15},
-            {tittle: "Passion Berry", "image": "passionberry.jpeg", "price": 3.99},
-            {tittle: "Oreo Cup", "image":"oreocup.jpg", "price" :2.99},
-          ];
-
-
 
 // Challenge 4: Create an array of JSON where each JSON contains the name, an image source and price for each Meat item.  
 // Solution to Challenge 4 is provided below as an example in order to complete Challenges 5 and 6.
+let Meats = [
+  { title: "Beef Tips", image: "beeftips.jpg", price: 18.99 },
+  { title: "Brisket", image: "brisket.jpg", price: 27.99 },
+  { title: "Ribeye", image: "ribeye.jpg", price: 21.99 },
+  { title: "Steak", image: "steak.jpg", price: 15.99 }
+];
 
+let seafood = [
+  { title: "Clams", image: "clams.jpg", price: 8.99 },
+  { title: "Crabs", image: "crabs.jpg", price: 12.99 },
+  { title: "Lobster", image: "lobster.png", price: 30.50 },
+  { title: "Scallop", image: "scallops.jpg", price: 23.95 },
+  { title: "Shrimp", image: "shrimp.jpg", price: 10.99 },
+  { title: "Tuna", image: "tuna.jpg", price: 5.99 }
+            ];
+            
+let desserts = [
+  { title: "Flan", image: "flan.jpg", price: 4.15 },
+  { title: "Passion Berry", image: "passionberry.jpeg", price: 3.99 },
+  { title: "Oreo Cup", image: "oreocup.jpg", price: 2.99 }
+              ];
 // Challenge 5: Create an array of JSON where each JSON contains the name, an image source and price for each Seafood item.  
 
 
@@ -54,13 +50,21 @@ function showMeats(){
      Make the title a heading(h3), place the image in the middle, and make the price a paragraph.
      2) Change the title to "Meats". 
   */
- for (let i=0, i < meats.length)
+ for(let i = 0; i < Meats.length; i++){
+    build += `<div class="card">
+                <h3> ${Meats[i].title}</h3>
+                <img src ="images/${Meats[i].image}"
+                <p>$${Meats[i].price}</p>
+            </div>`
+
+ }
 
 
   output.innerHTML = build;
   // Write the title "Meats" in the div with id 'title'
   title.innerHTML = "Meats";
 }
+
 
 function showSeafood(){
   let title = document.getElementById("title");
@@ -71,14 +75,22 @@ function showSeafood(){
      Make the title a heading(h3), place the image in the middle, and make the price a paragraph.   
      2) Change the title to "Seafood".
   */
+for(let i = 0; i < seafood.length; i++){
+    build += `<div class="card">
+                <h3> ${seafood[i],title}</h3>
+                <img src="images/${seafood[i].image}"
+                <p>$${seafood[i].price}</p>
+            </div>`
+          }
+
 
 
   output.innerHTML = build;
   title.innerHTML = "Seafood";
-
   // Write the title "Seafood" in the div with id 'title'
 
 }
+
 
 function showDesserts(){
   let title = document.getElementById("title");
@@ -90,10 +102,20 @@ function showDesserts(){
      2) Change the title to "Desserts".
   */
 
+for(let i = 0; i < desserts.length; i++){
+    build += `<div class="card">
+                <h3> ${desserts[i],title}</h3>
+                <img src="images/${desserts[i].image}"
+                <p>$${desserts[i].price}</p>
+            </div>`
+          }
+
+
+
+
 
   output.innerHTML = build;
   title.innerHTML = "Desserts";
-
   // Write the title "Desserts" in the div with id 'title'
 
 }
